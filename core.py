6e236a7ca092ff7bdb292a7c172703b24c691ce8
Rederick Deathwill <rederickdeathwill@gmail.com>
2017-02-19 00:28:37 -0300

    SysFont added.

--- core.py
+++ core.py
@@ -23,7 +23,29 @@ class Rectangle(Shape):
 		
 	def draw(self, surface):
 		pygame.draw.rect(surface, self.color, pygame.Rect(self.x, self.y, self.w, self.h))
+		
+class Ellipse(Shape):
 	
+	def __init__(self, color, pos, dimensions):
+		Shape.__init__(self, pos, dimensions)
+		self.color = color
+		
+	def draw(self, surface):
+		pygame.draw.ellipse(surface, self.color, pygame.Rect(self.x, self.y, self.w, self.h))
+		
+class SysFont(Shape):
+	
+	def __init__(self, color, pos, dimensions, font_name="monospace", bold=False, italic=False):
+		Shape.__init__(self, pos, dimensions)
+		self.color = color
+		self.font_name = font_name
+		self.bold = bold
+		self.italic = italic
+		self.font = pygame.font.SysFont(font_name, 32, bold, italic)
+
+	def draw_text(self, surface, text):
+		surface.blit(self.font.render(text, 1, self.color, (self.w, self.h)), (self.x, self.y))
+
 if __name__ == "__main__":	
 
 	pass
--- examples/0.py
+++ examples/0.py
@@ -7,6 +7,7 @@ WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 728
 
 pygame.init()
+pygame.font.init
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 FPS = 60
@@ -19,12 +20,14 @@ if __name__ == "__main__":
 			if e.type == pygame.QUIT:
 				sys.exit()
 
-	r = core.Rectangle((255, 255, 255), (0, 0), (100, 100))
+	r = core.Ellipse((255, 255, 255), (0, 0), (100, 100))
 	r.center((WINDOW_WIDTH, WINDOW_HEIGHT))
+	f = core.SysFont((255, 255, 255), (0, 0), (100, 100), "monospace", False, True)
 
 	def redraw():
 		pygame.display.flip()
 		screen.fill((0, 0, 0))
+		f.draw_text(screen, "Whatever")
 		r.draw(screen)
 
 while (running):
