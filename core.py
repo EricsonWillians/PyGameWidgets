57c7839a20db5a6eb7a363028336d80516991530
Rederick Deathwill <rederickdeathwill@gmail.com>
2017-02-18 23:12:51 -0300

    Uploading

---
+++ core.py
@@ -0,0 +1,24 @@
+import pygame
+import math
+
+class Entity:
+	
+	def __init__(self, name)
+		self.name = name
+		
+class Shape(Entity):
+	
+	def __init__(self, x, y, w, h):
+		Entity.__init__(self, "Shape")
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		
+class Rectangle(Shape):
+	
+	def __init__(self
+	
+if __name__ == "__main__":	
+
+	pass
---
+++ examples/0.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append("..")
+import core	
+
+if __name__ == "__main__":	
+
+	pass
